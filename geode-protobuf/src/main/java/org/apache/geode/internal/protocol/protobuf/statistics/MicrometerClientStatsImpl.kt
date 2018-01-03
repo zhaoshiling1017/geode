cfcc175cdf62f2bf2a7c72e09ad6e591579f05a1
Udo Kohlmeyer <ukohlmeyer@pivotal.io>
2018-01-03 09:49:54 -0800

    Changing counter to gauge

--- geode-protobuf/src/main/java/org/apache/geode/internal/protocol/protobuf/statistics/MicrometerClientStatsImpl.kt
+++ geode-protobuf/src/main/java/org/apache/geode/internal/protocol/protobuf/statistics/MicrometerClientStatsImpl.kt
@@ -1,19 +1,18 @@
 package org.apache.geode.internal.protocol.protobuf.statistics
 
-import com.netflix.spectator.atlas.AtlasConfig
-import io.micrometer.atlas.AtlasMeterRegistry
 import io.micrometer.core.instrument.Clock
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry
-import io.micrometer.core.instrument.util.HierarchicalNameMapper
-import io.micrometer.graphite.GraphiteConfig
-import io.micrometer.graphite.GraphiteMeterRegistry
 import io.micrometer.influx.InfluxConfig
 import io.micrometer.influx.InfluxMeterRegistry
 import org.apache.geode.internal.protocol.statistics.ProtocolClientStatistics
 import java.time.Duration
+import java.util.concurrent.atomic.AtomicInteger
 
 class MicrometerClientStatsImpl : ProtocolClientStatistics {
+
+    private val clientsConnected = AtomicInteger(0)
+
     private val influxMetrics: MeterRegistry = InfluxMeterRegistry(object : InfluxConfig {
         override fun step(): Duration = Duration.ofSeconds(10)
         override fun db(): String = "mydb"
@@ -21,34 +20,32 @@ class MicrometerClientStatsImpl : ProtocolClientStatistics {
         override fun uri(): String = "http://localhost:8086"
     }, Clock.SYSTEM)
 
-    private val atlasMetrics: MeterRegistry = AtlasMeterRegistry(object : AtlasConfig {
-        override fun get(k: String?): String? = null
-        override fun enabled(): Boolean = true
-        override fun uri(): String = "http://localhost:7101/api/v1/publish"
-        override fun step(): Duration = Duration.ofSeconds(10)
-    }, Clock.SYSTEM)
+//    private val atlasMetrics: MeterRegistry = AtlasMeterRegistry(object : AtlasConfig {
+//        override fun get(k: String?): String? = null
+//        override fun enabled(): Boolean = true
+//        override fun uri(): String = "http://localhost:7101/api/v1/publish"
+//        override fun step(): Duration = Duration.ofSeconds(10)
+//    }, Clock.SYSTEM)
 
     private val metrics = CompositeMeterRegistry(Clock.SYSTEM)
 
     init {
         metrics.add(influxMetrics)
-        metrics.add(atlasMetrics)
+//        metrics.add(atlasMetrics)
     }
 
-    val clientConnectedCounter = metrics.counter("clientConnected")
+    val clientConnectedCounter = metrics.gauge("clientConnected", clientsConnected)
     val messageReceivedCounter = metrics.counter("messageReceived")
     val messageSentCounter = metrics.counter("messageSent")
     val authorizationViolationsCounter = metrics.counter("authorizationViolations")
     val authenticationFailureCounter = metrics.counter("authenticationFailures")
 
     override fun clientConnected() {
-        System.err.println("Increment Counter")
-        clientConnectedCounter.increment()
+        clientsConnected.incrementAndGet()
     }
 
     override fun clientDisconnected() {
-        System.err.println("Decrement Counter")
-        clientConnectedCounter.increment(-1.0)
+        clientsConnected.decrementAndGet()
     }
 
     override fun messageReceived(bytes: Int) {
